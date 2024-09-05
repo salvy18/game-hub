4b894e0af5fa9e058f0546446fcf95ae74334c49
Rod Selmo <salvy18@hotmail.com>
2024-09-05 11:17:39 -0400

    Buld responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  background,
+  Button,
+  ButtonGroup,
+  Grid,
+  GridItem,
+  Show,
+} from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      {/* template areas what you see in "" means first row then the second means the second row */}
+      {/* Lets hide the left panel for small devices*/}
+      <Grid
+        templateAreas={{
+          base: `"nav rightmain"`, // it will use this by default but if is > than 1024 will use the second
+          lg: `"nav nav" "leftside rightmain"`, // 1024px it will use this when > 1024
+        }}
+      >
+        <GridItem area="nav" bg="coral">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="leftside" bg="gold">
+            Left Area
+          </GridItem>
+        </Show>
+        <GridItem area="rightmain" bg="dodgerblue">
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
-import "./index.css";
+// import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
